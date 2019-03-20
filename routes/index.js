a8b12f3fdd817a14b074e1f791dadd32724f8bb6
Andreas <andreas848@hotmail.com>
2019-03-20 09:45:34 +0100

    info

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var apiInfo = {
-  title: "Site with a simple users and error API",
+  title: "Site with a simple API for users & blog and an error",
   REST_allUsers : "Get all users like this: /api/users",
-  REST_UserName : "Get all users like this: /api/users/username=ckw",
-  REST_Userid : "Get all users like this: /api/users/id=5c8b9b5319d0bd2494d95e17",
+  REST_userName : "Get a user from username like this: /api/users/username=ckw",
+  REST_userid : "Get a user from id like this: /api/users/id=5c8b9b5319d0bd2494d95e17",
+  REST_allBlogs : "Get all blogs like this: /api/blogs",
+  REST_blogid : "Get a blog from id like this: /api/blogs/id=5c8b9b5319d0bd2494d95e1a",
   REST_error : "Get an error like this: /api/error",
 }
 
