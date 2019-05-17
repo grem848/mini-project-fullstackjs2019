0c89fbe705fa0b063e67b883fc4430d5670cf866
cl0tCh <screenchok@gmail.com>
2019-05-17 12:58:11 +0200

    Update testLocationBlog.js

--- test/testLocationBlog.js
+++ test/testLocationBlog.js
@@ -1,69 +1,70 @@
-const mongoose = require("mongoose");
-const expect = require("chai").expect;
-var connect = require("../dbConnect.js");
-
+const mongoose = require('mongoose');
+const expect = require('chai').expect;
+var connect = require('../dbConnect.js');
 
 //See (for the three lines below): https://github.com/Automattic/mongoose/issues/1251
 // mongoose.models = {};
 // mongoose.modelSchemas = {};
 // mongoose.connection = {};
 
-var blogFacade = require("../facades/blogFacade");
-var userFacade = require("../facades/userFacade");
-var LocationBlog = require("../models/LocationBlog");
+var blogFacade = require('../facades/blogFacade');
+var userFacade = require('../facades/userFacade');
+var LocationBlog = require('../models/LocationBlog');
 
-describe("Testing the LocationBlog Facade", function () {
+describe('Testing the LocationBlog Facade', function() {
+	/* Connect to the TEST-DATABASE */
+	before(async function() {
+		//this.timeout(require("../settings").MOCHA_TEST_TIMEOUT);
+		await connect(require('../settings').TEST_DB_URI);
+	});
 
-    /* Connect to the TEST-DATABASE */
-    before(async function () {
-        //this.timeout(require("../settings").MOCHA_TEST_TIMEOUT);
-        await connect(require("../settings").TEST_DB_URI);
-    })
+	after(async function() {
+		await mongoose.disconnect();
+	});
 
-    after(async function () {
-        await mongoose.disconnect();
-    })
+	var locationBlogs = [];
+	var users = [];
+	/* Setup the database in a known state (2 users) BEFORE EACH test */
+	beforeEach(async function() {
+		await LocationBlog.deleteMany({});
+		users = await userFacade.getAllUsers();
+		locationBlogs = await LocationBlog.insertMany([
+			{ info: 'Cool Place1', img: 'img.png', pos: { longitude: 26, latitude: 57 }, author: users[0]._id },
+			{ info: 'Cool Place2', img: 'img.png', pos: { longitude: 26, latitude: 57 }, author: users[0]._id }
+		]);
+	});
 
-    var locationBlogs = [];
-    var users = [];
-    /* Setup the database in a known state (2 users) BEFORE EACH test */
-    beforeEach(async function () {
-        await LocationBlog.deleteMany({});
-        users = await userFacade.getAllUsers();
-        locationBlogs = await LocationBlog.insertMany([
-            { info: "Cool Place1", img: "img.png", pos: { longitude: 26, latitude: 57 }, author: users[0]._id },
-            { info: "Cool Place2", img: "img.png", pos: { longitude: 26, latitude: 57 }, author: users[0]._id }
-        ]);
-    })
+	it('Should find all locationblogs (Cool Place1 & Cool Place2)', async function() {
+		var locationBlogs = await blogFacade.getAllLocationBlogs();
+		expect(locationBlogs.length).to.be.equal(2);
+	});
 
-    it("Should find all locationblogs (Cool Place1 & Cool Place2)", async function () {
-        var locationBlogs = await blogFacade.getAllLocationBlogs();
-        expect(locationBlogs.length).to.be.equal(2);
-    });
+	it('Should Find Cool Place1 by ID', async function() {
+		var locationBlog = await blogFacade.findById(locationBlogs[0]._id);
+		expect(locationBlog.info).to.be.equal('Cool Place1');
+	});
 
-    it("Should Find Cool Place1 by ID", async function () {
-        var locationBlog = await blogFacade.findById(locationBlogs[0]._id);
-        expect(locationBlog.info).to.be.equal("Cool Place1");
-    });
+	it('Should add Cool Place3', async function() {
+		var locationBlog = await blogFacade.addLocationBlog(
+			'Cool Place3',
+			'img.png',
+			{ longitude: 11, latitude: 12 },
+			users[1]._id
+		);
+		expect(locationBlog).to.not.be.null;
+		expect(locationBlog.info).to.be.equal('Cool Place3');
+		var locationBlogs = await blogFacade.getAllLocationBlogs();
+		expect(locationBlogs.length).to.be.equal(3);
+	});
 
-    it("Should add Cool Place3", async function () {
-        var locationBlog = await blogFacade.addLocationBlog("Cool Place3", "img.png", {longitude: 11, latitude: 12}, users[1]._id);
-        expect(locationBlog).to.not.be.null;
-        expect(locationBlog.info).to.be.equal("Cool Place3");
-        var locationBlogs = await blogFacade.getAllLocationBlogs();
-        expect(locationBlogs.length).to.be.equal(3);
-    });
-
-    it("Should like Cool Place2 for Kurt Wonnegut", async function () {
-        var locationBlogs = await blogFacade.getAllLocationBlogs();
-        var locationBlog = await blogFacade.findById(locationBlogs[1]._id);
-        expect(locationBlog.likedBy).to.be.empty;
-        locationBlog = await blogFacade.likeLocationBlog(locationBlogs[1]._id, users[0]._id);
-        expect(locationBlog).to.not.be.null;
-        expect(locationBlog.info).to.be.equal("Cool Place2");
-        expect(locationBlogs.length).to.be.equal(2);
-        expect(locationBlog.likedBy).to.be.contains(users[0]._id);
-    });
-    
-
-})
+	it('Should like Cool Place2 for Kurt Wonnegut', async function() {
+		var locationBlogs = await blogFacade.getAllLocationBlogs();
+		var locationBlog = await blogFacade.findById(locationBlogs[1]._id);
+		expect(locationBlog.likedBy).to.be.empty;
+		locationBlog = await blogFacade.likeLocationBlog(locationBlogs[1]._id, users[0]._id);
+		expect(locationBlog).to.not.be.null;
+		expect(locationBlog.info).to.be.equal('Cool Place2');
+		expect(locationBlogs.length).to.be.equal(2);
+		expect(locationBlog.likedBy).to.be.contains(users[0]._id);
+	});
+});
